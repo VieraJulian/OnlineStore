01568b157b9141dee1ddc7c80ca65a2fce9405b0
VieraJulian <julianviera2019@hotmail.com>
2024-01-08 17:31:20 -0300

    Julián - Configuración de un servidor centralizado

---
+++ configserver/src/test/java/com/onlineestore/configserver/ConfigserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onlineestore.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
